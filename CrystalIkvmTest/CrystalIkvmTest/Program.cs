d89156d6d46ad97e6d775ffc020d7b2a24733805
Peter Gill <peter@majorsilence.com>
2014-11-16 11:37:56 -0330

    Include simple crystal report file and code to export to pdf.

--- CrystalIkvmTest/CrystalIkvmTest/Program.cs
+++ CrystalIkvmTest/CrystalIkvmTest/Program.cs
@@ -6,7 +6,42 @@ namespace CrystalIkvmTest
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var data = GetReportSimpleAsBytes();
         }
+
+        private static byte[] GetReportSimpleAsBytes()
+        {
+            // see the java_crj12_web_resultset_datasource.jsp exmpale
+
+            java.sql.Connection cn;
+            java.sql.PreparedStatement statement;
+            java.sql.ResultSet resultset;
+            java.io.ByteArrayInputStream inputStream;
+            java.io.ByteArrayOutputStream outputStream = new java.io.ByteArrayOutputStream(); 
+            byte[] byteArray;
+            int bytesRead;
+
+
+            var rpt = new com.crystaldecisions.sdk.occa.report.application.ReportClientDocument();
+            rpt.setReportAppServer(com.crystaldecisions.sdk.occa.report.application.ReportClientDocument.inprocConnectionString);
+            rpt.open("thereport.rpt", com.crystaldecisions.sdk.occa.report.application.OpenReportOptions._openAsReadOnly);
+            var reportSource = rpt.getReportSource();
+
+            inputStream = (java.io.ByteArrayInputStream)rpt.getPrintOutputController().export(com.crystaldecisions.sdk.occa.report.exportoptions.ReportExportFormat.PDF);
+
+
+            byteArray = new byte[ 1024];
+            while ((bytesRead = inputStream.read(byteArray)) != -1)
+            {
+                outputStream.write(byteArray, 0, bytesRead);    
+
+            }
+
+            rpt.close();
+
+            return outputStream.toByteArray();
+        }
+
     }
 }
